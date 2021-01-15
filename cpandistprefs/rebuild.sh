2464cfeeb4f5e879faf6d3f6192e5d17b9d39ee2
David Cantrell <david@cantrell.org.uk>
2021-01-15 23:16:26 +0000

    just use whatever perl is first in the path

--- cpandistprefs/rebuild.sh
+++ cpandistprefs/rebuild.sh
@@ -3,6 +3,6 @@
 for i in *yml; do
     if [[ ! -s $i.dd || $i -nt $i.dd ]]; then
       echo Rebuilding $i
-      /usr/local/bin/perl -MYAML=LoadFile -MData::Dumper -e "print Dumper(LoadFile('$i'))">$i.dd
+      perl -MYAML=LoadFile -MData::Dumper -e "print Dumper(LoadFile('$i'))">$i.dd
     fi
 done
