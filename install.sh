c12a4378d256facaf984bfb6acb8f29d41f568a0
David Cantrell <david@cantrell.org.uk>
2022-02-17 23:16:11 +0000

    create symlink to charnames script

--- install.sh
+++ install.sh
@@ -38,6 +38,7 @@ function install {
 
     mkdir $HOME/bin 2>/dev/null
     install_symlink $HOME/bin/lls                $CHECKOUT_DIR/../perlscripts/lls
+    install_symlink $HOME/bin/charnames          $CHECKOUT_DIR/../perlscripts/charnames
 
     mkdir $HOME/.get_iplayer 2>/dev/null
     install_symlink $HOME/.get_iplayer/options   $CHECKOUT_DIR/get_iplayer/options
