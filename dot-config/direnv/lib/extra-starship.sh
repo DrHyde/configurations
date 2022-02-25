25b7d3ab2d29e530251e1fdfcf512e6eb6e93c1d
David Cantrell <david@cantrell.org.uk>
2022-02-25 22:34:37 +0000

    require less line-noise

--- dot-config/direnv/lib/extra-starship.sh
+++ dot-config/direnv/lib/extra-starship.sh
@@ -4,13 +4,12 @@
 # starship config plus the contents of that file, with the file
 # taking precedence, writes the results to a temp file, and sets
 # STARSHIP_CONFIG to the temp file. Use it in .envrc thus:
-#   extra-starship <(cat <<- EOF
+#   extra-starship '
 #     [nodejs]
-#     disabled="foish"
-#   EOF)
-# and NB that it's PICKY about whitespace
+#     somevariable = "fish"
+#   '
 
 extra-starship() {
     export STARSHIP_CONFIG=$(mktemp)
-    toml-merge ~/.config/starship.toml $1 >$STARSHIP_CONFIG
+    toml-merge ~/.config/starship.toml <(echo "$1"|sed 's/^[[:space:]]*//') >$STARSHIP_CONFIG
 }
