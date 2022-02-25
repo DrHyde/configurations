8521d9bd337cdc30759d31e51869ba9b55200c2e
David Cantrell <david@cantrell.org.uk>
2022-02-25 22:40:14 +0000

    fix explanatory comment

--- dot-config/direnv/lib/extra-starship.sh
+++ dot-config/direnv/lib/extra-starship.sh
@@ -1,7 +1,7 @@
 #!bash
 
-# this takes a filename on the cmd line and merges my default
-# starship config plus the contents of that file, with the file
+# this takes a multi-line string on the cmd line and merges my default
+# starship config plus the contents of that string, with the string
 # taking precedence, writes the results to a temp file, and sets
 # STARSHIP_CONFIG to the temp file. Use it in .envrc thus:
 #   extra-starship '
