dbdfaa076943b35abfc7f5b36434c4e0d64b1882
David Cantrell <david@cantrell.org.uk>
2024-04-23 00:28:13 +0100

    install: add ffduration and ffinfo scripts

--- install.sh
+++ install.sh
@@ -104,6 +104,8 @@ function install {
     install_symlink $HOME/bin/ffverify           $CHECKOUT_DIR/../shellscripts/ffverify
     install_symlink $HOME/bin/fps                $CHECKOUT_DIR/../shellscripts/fps
     install_symlink $HOME/bin/ffres              $CHECKOUT_DIR/../shellscripts/ffres
+    install_symlink $HOME/bin/ffduration         $CHECKOUT_DIR/../shellscripts/ffduration
+    install_symlink $HOME/bin/ffinfo             $CHECKOUT_DIR/../shellscripts/ffinfo
     install_symlink $HOME/bin/ffconcat           $CHECKOUT_DIR/../perlscripts/ffconcat
     install_symlink $HOME/bin/rotator            $CHECKOUT_DIR/../perlscripts/rotator
     install_symlink $HOME/bin/shtimeout          $CHECKOUT_DIR/../shellscripts/shtimeout
