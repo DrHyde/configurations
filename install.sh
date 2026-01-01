a0252b10192cfab9191114100a7c94b9d1381dc5
David Cantrell <david@cantrell.org.uk>
2026-01-01 23:28:49 +0000

    install: no longer want tldr everywhere

--- install.sh
+++ install.sh
@@ -312,7 +312,7 @@ function look_for_updates {
 
     wait
 
-    for wanted in dig tldr tree img2sixel cpulimit hyperfine hardlink fzf ctags ngrok karabiner duf diff-so-fancy trurl; do
+    for wanted in dig tree img2sixel cpulimit hyperfine hardlink fzf ctags ngrok karabiner duf diff-so-fancy trurl; do
         if [[ "$wanted" == "ngrok" && "$(uname)" =~ ^(SunOS|NetBSD|OpenBSD)$ ]]; then
             true
         elif [[ "$wanted" == "karabiner" ]]; then
