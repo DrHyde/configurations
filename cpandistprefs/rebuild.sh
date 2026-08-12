76d6a44ca76d442a8fef754d6bd4ea2fced9593e
David Cantrell <david@cantrell.org.uk>
2026-08-12 23:13:45 +0100

    cpandistprefs: auto-delete orphaned .yml.dd files

--- cpandistprefs/rebuild.sh
+++ cpandistprefs/rebuild.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
-for i in *yml; do
+for i in *.yml; do
     if [[ ! -s $i.dd || $i -nt $i.dd ]]; then
-      echo Rebuilding $i
-      perl -MYAML=LoadFile -MData::Dumper -e "print Dumper(LoadFile('$i'))">$i.dd
+        echo Rebuilding $i
+        perl -MYAML=LoadFile -MData::Dumper -e "print Dumper(LoadFile('$i'))">$i.dd
+    fi
+done
+
+for i in *.yml.dd; do
+    if [[ ! -f $(echo $i|sed 's/.dd$//') ]]; then
+        echo Deleting obsolete $i
+        rm $i
     fi
 done
